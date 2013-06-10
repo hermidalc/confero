652110f19f7649a2a03d6334ee0995859874b94c
Leandro Hermida <softdev@leandrohermida.com>
2013-06-09 21:07:35 -0400

    Set version to initial release 0.1 and modified help text to reflect new/changed commands

--- bin/cfo_run_cmd.pl
+++ bin/cfo_run_cmd.pl
@@ -13,7 +13,7 @@ sub sig_handler {
     die "\n\nConfero $0 command exited gracefully [", scalar localtime, "]\n\n";
 }
 
-our $VERSION = '0.8';
+our $VERSION = '0.1';
 
 my $cmd = shift @ARGV;
 pod2usage(
@@ -43,10 +43,11 @@ cfo_run_cmd.pl - Confero Command Runner
  Commands:
      process_data_file                  Check and process a data file (e.g. contrast data set, gene set list)
      process_submit_data_file           Check, process and submit a data file (e.g. contrast data set, gene set list)
-     create_ranked_lists                Create ranked list expression profiles from a contrast dataset or contrast file or one in Confero DB
+     create_rnk_deg_lists               Create ranked or DEG lists from a contrast dataset or contrast file or one in Confero DB
      analyze_data                       Analyze data for gene set enrichment using Confero DB, MSigDB, GeneSigDB, etc. gene set collections
      extract_gsea_leading_edge_matrix   Extract GSEA leading edge matrix from a GSEA result
      extract_gsea_results_matrix        Extract GSEA results data matrix from one or more GSEA results
+     extract_ora_results_matrix         Extract ORA results data matrix from one or more ORA results
      extract_gene_set_matrix            Extract gene set matrix from specific gene sets or one or more gene set databases
      extract_gene_set_overlap_matrix    Extract gene set overlap matrix from a gene set matrix or GSEA leading edge matrix
      extract_contrast_data_subset       Extract a subset of contrasts from an contrast dataset file or one in Confero DB
