f143dfe5e89519ef0727ccaba50abaeb8a2e6994
Leandro Hermida <softdev@leandrohermida.com>
2013-06-09 22:25:44 -0400

    Improved and more robust system setup program which includes new features

--- scripts/cfo_setup.pl
+++ scripts/cfo_setup.pl
@@ -5,17 +5,16 @@ use warnings;
 use FindBin;
 use lib "$FindBin::Bin/../lib/perl5", "$FindBin::Bin/../extlib/lib/perl5";
 use sigtrap qw(handler sig_handler normal-signals error-signals ALRM);
-use Confero::Config qw(:database);
-use Confero::LocalConfig qw($CTK_BASE_DIR :database :web);
+use Confero::LocalConfig qw(:database :web);
 use Confero::DB;
 use Getopt::Long qw(:config auto_help auto_version);
 use Pod::Usage qw(pod2usage);
-use Sys::Hostname::FQDN qw(fqdn);
+use Sys::Hostname qw(hostname);
 
 sub sig_handler {
     die "\n\n$0 program exited gracefully [", scalar localtime, "]\n\n";
 }
-our $VERSION = '0.0.1';
+our $VERSION = '0.1';
 # Unbuffer error and output streams (make sure STDOUT is last so that it remains the default filehandle)
 select(STDERR); $| = 1;
 select(STDOUT); $| = 1;
@@ -25,33 +24,32 @@ my $no_db = 0;
 my $no_download = 0;
 my $debug = 0;
 my $verbose = 0;
-my $man = 0;
 GetOptions(
     'no-interactive' => \$no_interactive,
     'no-db'          => \$no_db,
     'no-download'    => \$no_download,
     'debug'          => \$debug,
     'verbose'        => \$verbose,
-    'man'            => \$man,
 ) || pod2usage(-verbose => 0);
-pod2usage(-exitstatus => 0, -verbose => 2) if $man;
-print "#", '-' x 100, "#\n",
+print "#", '-' x 120, "#\n",
       "# Confero System Setup [" . scalar localtime() . "]\n\n",
       "Welcome to the Confero system setup program!\n\n";
 # database setup
 print "[Database]\n";
 if (!$no_db) {
-    my $CURRENT_HOST = fqdn();
-    my ($REAL_WEB_SERVER_HOST) = $CTK_WEB_SERVER_HOST ne $CTK_DB_HOST ? gethostbyname($CTK_WEB_SERVER_HOST) : 'localhost';
-    $REAL_WEB_SERVER_HOST = 'localhost' if $REAL_WEB_SERVER_HOST eq 'localhost.localdomain';
+    my $hostname = hostname;
     my $DB_CREATE_STMTS =
     "CREATE DATABASE $CTK_DB_NAME DEFAULT CHARACTER SET = 'utf8'; \\
-     GRANT ALL PRIVILEGES ON ${CTK_DB_NAME}.* TO ${CTK_DB_USER}\@'${REAL_WEB_SERVER_HOST}' IDENTIFIED BY '${CTK_DB_PASS}'; \\
-     GRANT ALL PRIVILEGES ON ${CTK_DB_NAME}.* TO ${CTK_DB_USER}\@'${CURRENT_HOST}' IDENTIFIED BY '${CTK_DB_PASS}';";
+     GRANT ALL PRIVILEGES ON ${CTK_DB_NAME}.* TO ${CTK_DB_USER}\@'%' IDENTIFIED BY '${CTK_DB_PASS}'; \\
+     GRANT ALL PRIVILEGES ON ${CTK_DB_NAME}.* TO ${CTK_DB_USER}\@'$hostname' IDENTIFIED BY '${CTK_DB_PASS}'; \\
+     GRANT ALL PRIVILEGES ON ${CTK_DB_NAME}.* TO ${CTK_DB_USER}\@'localhost' IDENTIFIED BY '${CTK_DB_PASS}'; \\
+     GRANT ALL PRIVILEGES ON ${CTK_DB_NAME}.* TO ${CTK_DB_USER}\@'127.0.0.1' IDENTIFIED BY '${CTK_DB_PASS}';";
     my $DB_DROP_STMTS =
     "DROP DATABASE $CTK_DB_NAME;";
-    #DROP USER IF EXISTS ${CTK_DB_USER}\@'${REAL_WEB_SERVER_HOST}'; \\
-    #DROP USER IF EXISTS ${CTK_DB_USER}\@'${CURRENT_HOST}';";
+    #DROP USER IF EXISTS ${CTK_DB_USER}\@'%'; \\
+    #DROP USER IF EXISTS ${CTK_DB_USER}\@'$hostname'; \\
+    #DROP USER IF EXISTS ${CTK_DB_USER}\@'localhost'; \\
+    #DROP USER IF EXISTS ${CTK_DB_USER}\@'127.0.0.1';";
     print "Welcome, first need elevated credentials for creating database and granting privileges...\n";
     print "DB superuser username [$ENV{USER}]: ";
     chomp(my $db_user = <STDIN>);
@@ -121,10 +119,11 @@ if (!$no_db) {
     # deploy schema
     my $ctk_db = Confero::DB->new();
     print "Deploying schema\n\n";
-    my @deploy_stmts = $ctk_db->deployment_statements(undef, undef, undef, $CTK_DB_SQLT_ARGS);
+    my $mysql_sqlt_args = { show_warnings => 1, producer_args => { mysql_version =>  $ctk_db->storage->dbh->selectrow_array('SELECT VERSION()') } };
+    my @deploy_stmts = $ctk_db->deployment_statements(undef, undef, undef, $mysql_sqlt_args);
     print "$deploy_stmts[0]", join(";\n", @deploy_stmts[1 .. $#deploy_stmts - 1]), ";\n--\n--\n$deploy_stmts[$#deploy_stmts];\n--\n--\n\n" if $verbose or $debug;
-    # deploy doesn't return true/false on success/failure
-    $ctk_db->deploy($CTK_DB_SQLT_ARGS); #or die "\nError, could not successfully deploy the Confero schema\n\n";
+    # DBIx::Class schema deploy unfortunately doesn't return true/false on success/failure
+    $ctk_db->deploy($mysql_sqlt_args); #or die "\nError, could not successfully deploy the Confero schema\n\n";
 }
 else {
     # Check if database exists using configured credentials 
@@ -142,27 +141,30 @@ else {
 }
 # run admin management scripts
 print "[Programs]\n",
-      "Running management programs to download and generate required system files and to initialize database\n\n";
+      "Running management programs to download and generate required system files and initialize database\n\n";
 my $cmd_opts = '';
 $cmd_opts .= ' --no-interactive' if $no_interactive;
 $cmd_opts .= $no_download 
-           ? ' --no-entrez-download' 
+           ? ' --no-entrez-download'
            : ' --download-netaffx --download-agilent --download-geo --download-illumina';
 $cmd_opts .= ' --debug' if $debug;
 $cmd_opts .= ' --verbose' if $verbose;
-system("$FindBin::Bin/cfo_load_entrez_gene_mapping_reprocess.pl $cmd_opts") == 0 or die "\nCommand failed! Exit code: ", $? >> 8, "\n\n";
-system("$FindBin::Bin/cfo_build_msigdb_c2_ar_gmt.pl") == 0 or die "\nCommand failed! Exit code: ", $? >> 8, "\n\n";
-system("$FindBin::Bin/cfo_fetch_process_genesigdb.pl") == 0 or die "\nCommand failed! Exit code: ", $? >> 8, "\n\n";
-system("$FindBin::Bin/cfo_create_gsdb_gene_set_galaxy_opts.pl") == 0 or die "\nCommand failed! Exit code: ", $? >> 8, "\n\n";
-system("$FindBin::Bin/cfo_define_organisms.pl") == 0 or die "\nCommand failed! Exit code: ", $? >> 8, "\n\n";
-
-print "[Web Server]\nStarting web server...\n";
-
-# open xml file and replace 'DANCER_URL' with the LocalConfig variable $CTK_WEB_SERVER_HOST
-my $view_manage_data_file = "$CTK_BASE_DIR/galaxy/view_manage_data.xml";
-system("sed -i 's!DANCER_URL!$CTK_WEB_SERVER_HOST!g' $view_manage_data_file") == 0 or die "\nFailed to define DANCER_URL in $view_manage_data_file ! Exit code: ", $? >> 8, "\n\n";
-
-system("$FindBin::Bin/cfo_web_app.pl start production") == 0 or die "\nCommand failed! Exit code: ", $? >> 8, "\n\n";
+system("$FindBin::Bin/cfo_load_entrez_gene_mapping_reprocess.pl $cmd_opts") == 0 
+    or die "\nCommand failed! Exit code: ", $? >> 8, "\n\n";
+system("$FindBin::Bin/cfo_build_msigdb_c2_ar_gmt.pl") == 0 
+    or warn "\nCommand failed! Exit code: ", $? >> 8, "\n\n";
+system("$FindBin::Bin/cfo_fetch_process_genesigdb.pl") == 0 
+    or warn "\nCommand failed! Exit code: ", $? >> 8, "\n\n";
+system("$FindBin::Bin/cfo_create_gsdb_gene_set_galaxy_opts.pl") == 0 
+    or warn "\nCommand failed! Exit code: ", $? >> 8, "\n\n";
+system("$FindBin::Bin/cfo_define_organisms.pl") == 0 
+    or warn "\nCommand failed! Exit code: ", $? >> 8, "\n\n";
+print "[Web Server]\n";
+# open Galaxy xml file and replace 'DANCER_URL' with the LocalConfig variable $CTK_WEB_SERVER_HOST
+system("sed -i 's!CONFERO_WEB_SERVER_URL!$CTK_WEB_SERVER_HOST!g' $FindBin::Bin/../galaxy/view_manage_data.xml") == 0 
+    or warn "\nFailed to find CONFERO_WEB_SERVER_URL in $FindBin::Bin/../galaxy/view_manage_data.xml! Exit code: ", $? >> 8, "\n\n";
+system("$FindBin::Bin/cfo_app_server.pl start -h $CTK_WEB_SERVER_HOST -p $CTK_WEB_SERVER_PORT") == 0 
+    or warn "\nCommand failed! Exit code: ", $? >> 8, "\n\n";
 print "\nConfero system setup complete, platform is ready to use!\n\n";
 exit;
 
@@ -179,8 +181,9 @@ cfo_setup.pl - Confero System Setup
  Options:
      --no-interactive          Run in non-interactive mode (default false)
      --no-db                   Skip database (re)creation step (default false)
+     --debug                   Enable debug mode (default off)
+     --verbose                 Be verbose (default off)
      --help                    Display usage and exit
-     --man                     Display full program documentation
      --version                 Display program version and exit
 
 =cut
