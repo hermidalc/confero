6b0ab8023087b1fd4b6c582ef89ea5e1eb122d55
Leandro Hermida <softdev@leandrohermida.com>
2013-06-09 22:40:23 -0400

    Removed unnecessary extlib and lib symlinks and changed version to 0.1

--- www/bin/app.pl
+++ www/bin/app.pl
@@ -1,10 +1,10 @@
 #!/usr/bin/env perl
 
 use FindBin;
-use lib "$FindBin::Bin/../lib/perl5", "$FindBin::Bin/../extlib/lib/perl5";
+use lib "$FindBin::Bin/../../lib/perl5", "$FindBin::Bin/../../extlib/lib/perl5";
 use Dancer;
 use Confero::Web;
 
-our $VERSION = '0.0.1';
+our $VERSION = '0.1';
 
 dance;
