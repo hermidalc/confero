987239c15f7934974f563fdfaf5d094a8f575b60
Leandro Hermida <softdev@leandrohermida.com>
2013-06-09 22:10:32 -0400

    Updated version to initial release 0.1 and schema dump code to new dynamic determination of MySQL server version

--- misc/cfo_dump_db_ddl.pl
+++ misc/cfo_dump_db_ddl.pl
@@ -5,14 +5,13 @@ use warnings;
 use FindBin;
 use lib "$FindBin::Bin/../lib/perl5", "$FindBin::Bin/../extlib/lib/perl5";
 use sigtrap qw(handler sig_handler normal-signals error-signals ALRM);
-use Confero::Config qw(:database);
 use Confero::DB;
 
 sub sig_handler {
     die "\n\n$0 program exited gracefully [", scalar localtime, "]\n\n";
 }
 
-our $VERSION = '0.0.1';
+our $VERSION = '0.1';
 
 # Unbuffer error and output streams (make sure STDOUT is last so that it remains the default filehandle)
 select(STDERR); $| = 1;
@@ -20,5 +19,7 @@ select(STDOUT); $| = 1;
 
 # dump schema DDL
 my $ctk_db = Confero::DB->new();
-my @deploy_stmts = $ctk_db->deployment_statements(undef, undef, undef, $CTK_DB_SQLT_ARGS);
+my $mysql_sqlt_args = { show_warnings => 1, producer_args => { mysql_version =>  $ctk_db->storage->dbh->selectrow_array('SELECT VERSION()') } };
+my @deploy_stmts = $ctk_db->deployment_statements(undef, undef, undef, $mysql_sqlt_args);
 print "$deploy_stmts[0]", join(";\n", @deploy_stmts[1 .. $#deploy_stmts - 1]), ";\n--\n--\n$deploy_stmts[$#deploy_stmts];\n--\n--\n\n";
+exit;
