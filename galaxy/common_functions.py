d3f0d770f76fcb9abbd5907e06b9abe601d0ce9b
Leandro Hermida <leandro.hermida@nih.gov>
2017-01-02 14:49:09 -0500

    Update confero_platform -> confero

--- galaxy/common_functions.py
+++ galaxy/common_functions.py
@@ -4,10 +4,10 @@ import sys, os, subprocess, json
 def cfo_get_info(data_type, with_empty=False, annotation_names=''):
     cmd_opts = [ '--as-json', '--as-tuples' ]
     if with_empty: cmd_opts += [ '--with-empty' ]
-    pipe = subprocess.Popen([ os.path.abspath(os.path.dirname(sys.argv[0])) + '/../tools/confero_platform/bin/cfo_get_info.pl' ] + cmd_opts + [ data_type, annotation_names ], stdout=subprocess.PIPE)
+    pipe = subprocess.Popen([ os.path.abspath(os.path.dirname(sys.argv[0])) + '/../tools/confero/bin/cfo_get_info.pl' ] + cmd_opts + [ data_type, annotation_names ], stdout=subprocess.PIPE)
     return json.loads(pipe.stdout.read())
 
 def cfo_get_contrast_info_from_dataset(source, source_value):
-    pipe = subprocess.Popen([ os.path.abspath(os.path.dirname(sys.argv[0])) + '/../tools/confero_platform/galaxy/cfo_get_contrast_info_from_dataset.pl', '--as-tuples', '--get-idxs', '--dataset-' + ('file' if source == 'from_file' else 'id') + '=' + source_value ], stdout=subprocess.PIPE)
+    pipe = subprocess.Popen([ os.path.abspath(os.path.dirname(sys.argv[0])) + '/../tools/confero/galaxy/cfo_get_contrast_info_from_dataset.pl', '--as-tuples', '--get-idxs', '--dataset-' + ('file' if source == 'from_file' else 'id') + '=' + source_value ], stdout=subprocess.PIPE)
     return json.loads(pipe.stdout.read())
 
--- galaxy/get_r_contrasts.py
+++ galaxy/get_r_contrasts.py
@@ -4,7 +4,7 @@ def get_contrast_list(file_selected):
 	# Call the process for R
 	f = open('log.txt', 'w')
 	f.write('file_selected: ' + file_selected + '\n')
-	runme = 'R --vanilla --slave --args ' + file_selected + ' < ' + os.path.abspath(os.path.dirname(sys.argv[0])) + '/../tools/confero_platform/galaxy/get_r_contrasts.R' 
+	runme = 'R --vanilla --slave --args ' + file_selected + ' < ' + os.path.abspath(os.path.dirname(sys.argv[0])) + '/../tools/confero/galaxy/get_r_contrasts.R'
 	f.write('Runme: ' + runme + '\n')
 	process = subprocess.Popen(runme, shell=True, stdout=subprocess.PIPE)
 	stdout_value = process.communicate()[0]
